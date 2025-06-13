9ab42be199d18440b883d5d1df97d4c2a11f8f28
leonard <leonard@pixelfreestudio.com>
2025-06-14 01:06:02 +0200

    initial commit

---
+++ scripts/world_generation.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@export var scene_to_instance: PackedScene
+@export var count: int = 8
+@export var radius: float = 200.0
+
+func _ready():
+	for i in range(count):
+		var instance = scene_to_instance.instantiate()
+		var angle = TAU * i / count
+		instance.position = Vector2(cos(angle), sin(angle)) * radius
+		add_child(instance)
