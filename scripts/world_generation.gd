e8a8974ec62c926c145a8e89c295c4e9f1b3c6cc
indifferenteye <leonard@pixelfreestudio.com>
2025-06-14 19:35:01 +0200

    inspector change: star system and sun object

--- scripts/world_generation.gd
+++ scripts/world_generation.gd
@@ -20,19 +20,19 @@ extends Node2D
 var rng := RandomNumberGenerator.new()
 
 func _ready():
-                rng.seed = seed
-                generate_spiral_galaxy()
+				rng.seed = seed
+				generate_spiral_galaxy()
 
 ## Generates a simple spiral galaxy. Adjust exported variables to
 ## tweak the resulting shape.
 func generate_spiral_galaxy() -> void:
-                for i in range(star_count):
-                                var instance = scene_to_instance.instantiate()
-                                var t := float(i) / star_count
-                                var arm := rng.randi() % arm_count
-                                var r := t * radius + rng.randf_range(-random_offset, random_offset)
-                                var angle := t * twist + TAU * arm / arm_count
-                                angle += rng.randf_range(-arm_spread, arm_spread)
-                                instance.position = Vector2(cos(angle), sin(angle)) * r
-                                instance.seed = rng.randi()
-                                add_child(instance)
+				for i in range(star_count):
+								var instance = scene_to_instance.instantiate()
+								var t := float(i) / star_count
+								var arm := rng.randi() % arm_count
+								var r := t * radius + rng.randf_range(-random_offset, random_offset)
+								var angle := t * twist + TAU * arm / arm_count
+								angle += rng.randf_range(-arm_spread, arm_spread)
+								instance.position = Vector2(cos(angle), sin(angle)) * r
+								instance.seed = rng.randi()
+								add_child(instance)
