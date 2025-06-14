b9a6ceb3f2268e2a1cf83dbf954c911c664063fe
indifferenteye <contact@indifferenteye.com>
2025-06-14 19:40:08 +0200

    Make stars open star system when clicked

---
+++ scripts/star_system.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+## Scene used for the system's star.
+@export var sun_scene: PackedScene = preload("res://assets/sun.tscn")
+
+func _ready() -> void:
+    var sun: Node2D = sun_scene.instantiate()
+    add_child(sun)
+    sun.position = Vector2.ZERO
