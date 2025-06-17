0625bfadd847f0acfe85048585c4aa0141403a66
indifferenteye <contact@indifferenteye.com>
2025-06-17 22:15:34 +0200

    Add asteroid group connections

--- scripts/asteroid_belt.gd
+++ scripts/asteroid_belt.gd
@@ -27,6 +27,7 @@ func _generate_asteroids() -> void:
     for i in range(asteroid_count):
         var asteroid: Node2D = asteroid_scene.instantiate()
         add_child(asteroid)
+        asteroid.add_to_group("asteroid")
         var angle := rng.randf() * TAU
         var r := radius + rng.randf_range(-thickness / 2.0, thickness / 2.0)
         asteroid.position = Vector2(cos(angle), sin(angle)) * r
