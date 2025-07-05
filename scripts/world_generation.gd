2bff7210fa8888c03c0978f55c3f5856aaf3ffa0
indifferenteye <contact@indifferenteye.com>
2025-07-05 23:18:06 +0200

    Record galaxy drone counts when entering galaxy

--- scripts/world_generation.gd
+++ scripts/world_generation.gd
@@ -41,6 +41,7 @@ func _ready() -> void:
         Globals.first_load = false
     else:
         _spawn_all_drones()
+    _record_galaxy_drone_counts()
 
 ## Generates a simple spiral galaxy. Adjust exported variables to tweak the
 ## resulting shape.
